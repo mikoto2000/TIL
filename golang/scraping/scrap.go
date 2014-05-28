a39f51a4a5c42317b9ab873ae834590dad6cafce
mikoto2000 <mikoto2000@gmail.com>
2014-05-29 05:22:56 +0900

    Refactored scrap.

--- golang/scraping/scrap.go
+++ golang/scraping/scrap.go
@@ -3,32 +3,52 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"github.com/PuerkitoBio/goquery"
 )
 
-// NHK NEWS 
-var urls = []string{
-	"http://www3.nhk.or.jp/news/html/20140528/k10014806641000.html",
-	"http://t.co/N8sCvwUXNk"}
+// GetSelectionTexts() access urls and scraping selectors text.
+// TODO: Add waiting time.
+func GetSelectionTexts(urls []string, selectors []string) []string {
+	selectionTexts := []string{}
+
+	for _, url := range urls {
+		var buf bytes.Buffer
+
+		doc, _ := goquery.NewDocument(url)
+
+		for _, selector := range selectors {
+			doc.Find(selector).Each(
+				func(_ int, selection *goquery.Selection) {
+					buf.WriteString(selection.Text())
+					buf.WriteString("\n")
+				})
+		}
+
+		selectionTexts = append(selectionTexts, buf.String())
+
+	}
+
+	return selectionTexts
+}
 
 func main() {
-	for _, url := range urls {
-	doc, _ := goquery.NewDocument(url)
 
-	// Get main and more text.
-	doc.Find("span.contentTitle,p#news_textbody,p#news_textmore").Each(
-		func(_ int, selection *goquery.Selection) {
-			fmt.Println(selection.Text())
-		})
+	// NHK NEWS
+	urls := []string{
+		"http://www3.nhk.or.jp/news/html/20140528/k10014806641000.html",
+		"http://www3.nhk.or.jp/news/html/20140528/k10014803771000.html"}
 
-	// Gett add texts.
-	doc.Find("div.news_add").Each(
-		func(_ int, newsAdd *goquery.Selection) {
-			newsAdd.Find("h3,p").Each(
-				func(_ int, selection *goquery.Selection) {
-					fmt.Println(selection.Text())
-				})
-		})
+	selectors := []string{
+		"span.contentTitle,p#news_textbody,p#news_textmore",
+		"div.news_add h3, div.news_add p"}
+
+	texts := GetSelectionTexts(urls, selectors)
+
+	for _, text := range texts {
+		fmt.Println(text)
+		fmt.Println()
+		fmt.Println(text)
 	}
 }
