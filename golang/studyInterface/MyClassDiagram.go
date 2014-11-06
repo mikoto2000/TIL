9a947bfd559ba9847abc8cbd1173107d6a3e1d2a
mikoto2000 <mikoto2000@gmail.com>
2014-11-07 00:55:51 +0900

    Updated "MyClassDiagram.go".
    Add Method struct.

--- golang/studyInterface/MyClassDiagram.go
+++ golang/studyInterface/MyClassDiagram.go
@@ -4,25 +4,48 @@ import (
 	"fmt"
 )
 
+// Dot 形式の文字列に変換できることを表すインターフェース
 type Dot interface {
+	// Dot 形式の文字列を返却する
 	ToDot() string
 }
 
+// フィールド
 type Field struct {
 	field string
 }
 
+// フィールドを作成する
 func CreateFieldFromString(def string) Field {
 	return Field{def}
 }
 
+// Dot 形式の文字列を返却する
 func (this Field) ToDot() string {
 	return this.field
 }
 
-func main() {
-	var dot Dot;
-	dot = CreateFieldFromString("- field")
+// メソッド
+type Method struct {
+	method string
+}
 
-	fmt.Printf(dot.ToDot());
+// メソッドを作成する
+func CreateMethodFromString(def string) Method {
+	return Method{def}
+}
+
+// Dot 形式の文字列を返却する
+func (this Method) ToDot() string {
+	return this.method
+}
+
+func main() {
+	dot := [...]Dot{
+		CreateFieldFromString("- field : string"),
+		CreateMethodFromString("+ method() : string")}
+
+	for _, v := range dot {
+		fmt.Printf("%s\n", v.ToDot())
+	}
 }
