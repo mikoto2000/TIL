6a58d324de27467d21cf67d9421907712da5de15
mikoto2000 <mikoto2000@gmail.com>
2014-11-23 03:02:23 +0900

    Updated 43_FunctionClousures.

--- golang/TourOfGo/43_FunctionClousures.go
+++ golang/TourOfGo/43_FunctionClousures.go
@@ -6,20 +6,31 @@ import (
 
 // int の引数をもらって、
 // int を返す関数を返す関数。
+//
+// - addr() を呼び出すたびに、
+//   addr 空間(環境)が新しく作成される。
+// - addr 空間内で作成された関数は、
+//   その空間で宣言された変数を使用することが出来る。
+// - addr 空間内で宣言された変数は、
+//   他の空間から参照・変更されない。
+// という感じか？
 func adder() func(int) int {
 	sum := 0
-	return func(x int) int {
+
+	innerFunc := func(x int) int {
 		sum += x
 		return sum
 	}
+
+	return innerFunc
 }
 
 func main() {
-	pos, neg := adder(), adder()
-	for i := 0; i < 10; i++ {
-		fmt.Println(
-			pos(i),
-			neg(-2*i),
-		)
-	}
+	f1 := adder()
+	fmt.Println(f1(1))
+	fmt.Println(f1(1))
+
+	f2 := adder()
+	fmt.Println(f2(1))
+	fmt.Println(f2(1))
 }
