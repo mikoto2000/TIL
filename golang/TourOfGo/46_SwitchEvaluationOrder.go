38c258cd14dceeadb67c27f29ff0fd8d43ffe89c
mikoto2000 <mikoto2000@gmail.com>
2014-11-23 05:10:09 +0900

    Updated 46_SwitchEvaluationOrder.

--- golang/TourOfGo/46_SwitchEvaluationOrder.go
+++ golang/TourOfGo/46_SwitchEvaluationOrder.go
@@ -6,10 +6,10 @@ import (
 )
 
 func main() {
-	fmt.Println("When's Saturday?")
+	fmt.Println("When's Sunday?")
 	today := time.Now().Weekday()
 
-	switch time.Saturday {
+	switch time.Sunday {
 	case today + 0:
 		fmt.Println("Today.")
 	case today + 1:
