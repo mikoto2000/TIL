40b6bcb9fd96b791ec928a93139e4a99a2cff426
mikoto2000 <mikoto2000@gmail.com>
2014-11-23 04:05:29 +0900

    Updated 45_Switch.

--- golang/TourOfGo/45_Switch.go
+++ golang/TourOfGo/45_Switch.go
@@ -5,6 +5,23 @@ import (
 	"runtime"
 )
 
+func n(n int)  {
+	fmt.Printf("Input %v\n", n)
+	switch num := n; num {
+	case 0:
+		fmt.Printf("num == %v.\n", 0)
+	case 2:
+		fmt.Printf("num >= %v.\n", 2)
+		fallthrough
+	case 1:
+		fmt.Printf("num >= %v.\n", 1)
+		fallthrough
+	default:
+		fmt.Printf("num is %v.\n", num)
+	}
+	fmt.Println()
+}
+
 func main() {
 	fmt.Print("Go runs on ")
 	switch os := runtime.GOOS; os {
@@ -15,4 +32,9 @@ func main() {
 	default:
 		fmt.Printf("%s", os)
 	}
+
+	n(0)
+	n(1)
+	n(2)
+
 }
