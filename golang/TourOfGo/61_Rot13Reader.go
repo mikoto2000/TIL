d9a56607a68b61c6432a3088e96af493e2bc2daa
mikoto2000 <mikoto2000@gmail.com>
2014-11-25 05:31:02 +0900

    Added 61_Rot13Reader.
    Implemented pass through reader.

---
+++ golang/TourOfGo/61_Rot13Reader.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"io"
+	"os"
+	"strings"
+)
+
+type rot13Reader struct {
+	r io.Reader
+}
+
+func (reader rot13Reader) Read(p []byte) (n int, err error) {
+	return reader.r.Read(p)
+}
+
+func main() {
+	s := strings.NewReader(
+		"Lbh penpxrq gur pbqr!")
+	r := rot13Reader{s}
+	io.Copy(os.Stdout, &r)
+}
