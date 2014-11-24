049ac2821fb66c50d1297600f77bf13fdc71d238
mikoto2000 <mikoto2000@gmail.com>
2014-11-25 06:39:39 +0900

    Updated 67_Select.

--- golang/TourOfGo/67_Select.go
+++ golang/TourOfGo/67_Select.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func fibonacci(c, quit chan int) {
@@ -14,6 +15,7 @@ func fibonacci(c, quit chan int) {
 			fmt.Println("quit")
 			return
 		}
+		time.Sleep(1000 * time.Millisecond)
 	}
 }
 
@@ -21,11 +23,18 @@ func main() {
 	c := make(chan int)
 	quit := make(chan int)
 
+	// サブゴルーチンでフィボナッチの出力処理
 	go func (){
-		for i := 0; i < 10; i++ {
+		for i := 0; i < 11; i++ {
+			// c に値が送信されるまでブロック
 			fmt.Println(<-c)
 		}
+
+		// 10 個の値を受信したら
+		// fibonacci 関数を終了させる
 		quit <- 0
 	}()
+
+	// メインゴルーチンでフィボナッチ数列の計算
 	fibonacci(c, quit)
 }
