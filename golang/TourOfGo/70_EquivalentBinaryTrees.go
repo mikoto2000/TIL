2229a33dbddfb8e788bbfc6ee0fee83973f926ad
mikoto2000 <mikoto2000@gmail.com>
2014-11-26 01:57:14 +0900

    Updated 70_EquivalentBinaryTrees.
    Implemented Same method.

--- golang/TourOfGo/70_EquivalentBinaryTrees.go
+++ golang/TourOfGo/70_EquivalentBinaryTrees.go
@@ -20,13 +20,38 @@ func Walk(t *tree.Tree, ch chan int) {
 	}
 }
 
+// 1. t1, t2 でそれぞれ Walk 実行
+// 2. それぞれのチャネルには、
+//    値の少ない順番で値が格納されていくので、
+//    何位も考えずに順番に比較していって、
+//    不一致のものがあれば false を返却。
+func Same(t1, t2 *tree.Tree) bool {
+	t1Ch := make(chan int)
+	go Walk(t1, t1Ch)
+
+	t2Ch := make(chan int)
+	go Walk(t2, t2Ch)
+
+	for i := 0; i < 10; i++ {
+		if <-t1Ch != <-t2Ch {
+			return false
+		}
+	}
+
+	return true
+
+}
+
 func main() {
 
 	ch := make(chan int)
 
-	go Walk(tree.New(1), ch)
+	go Walk(tree.New(2), ch)
 
 	for i := 0; i < 10; i++ {
 		fmt.Println(<-ch)
 	}
+
+	fmt.Println(Same(tree.New(1), tree.New(1)))
+	fmt.Println(Same(tree.New(2), tree.New(1)))
 }
