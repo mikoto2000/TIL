3cae8d3c0bc558d573695e81cda33d20cbf9b968
mikoto2000 <mikoto2000@gmail.com>
2014-11-26 01:47:54 +0900

    Updated 70_EquivalentBinaryTrees.
    Implemented Walk method.

--- golang/TourOfGo/70_EquivalentBinaryTrees.go
+++ golang/TourOfGo/70_EquivalentBinaryTrees.go
@@ -5,6 +5,28 @@ import (
 	"fmt"
 )
 
+// 1. Left 探索
+// 2. 自分の値をチャネルへ
+// 3. Right 探索
+func Walk(t *tree.Tree, ch chan int) {
+	if t.Left != nil {
+		Walk(t.Left, ch)
+	}
+
+	ch <- t.Value
+
+	if t.Right != nil {
+		Walk(t.Right, ch)
+	}
+}
+
 func main() {
-	fmt.Println(tree.New(1))
+
+	ch := make(chan int)
+
+	go Walk(tree.New(1), ch)
+
+	for i := 0; i < 10; i++ {
+		fmt.Println(<-ch)
+	}
 }
