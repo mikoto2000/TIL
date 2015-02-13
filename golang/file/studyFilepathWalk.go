6b3eed322109723a093d66e3fe918b05c4bc68f8
mikoto2000 <mikoto2000@gmail.com>
2015-02-13 20:38:14 +0900

    Modified studyFilepathWalk in golang.
    指定された拡張子のものだけ表示するように修正。

--- golang/file/studyFilepathWalk.go
+++ golang/file/studyFilepathWalk.go
@@ -7,21 +7,28 @@ import (
 )
 
 // 第一引数で渡されたディレクトリ下を探索
+// 第二引数で渡された拡張子のファイルのみ表示する
 func main() {
+	if len(os.Args) != 3 {
+		os.Exit(0)
+	}
+
 	dir := os.Args[1]
+	ext := os.Args[2]
 
 	err := filepath.Walk(dir,
-		func (path string, info os.FileInfo, err error) error {
+		func(path string, info os.FileInfo, err error) error {
 			if info.IsDir() {
 				return nil
-		}
+			}
 
-		rel, err := filepath.Rel(dir, path)
+			if filepath.Ext(path) == "."+ext {
+				rel, _ := filepath.Rel(dir, path)
+				fmt.Println(rel)
+			}
 
-		fmt.Println(rel)
-
-		return nil
-	})
+			return nil
+		})
 
 	if err != nil {
 		fmt.Println(1, err)
