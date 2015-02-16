0eb7fca3d0ecbe94c5c15d0bc701eec627a55623
mikoto2000 <mikoto2000@gmail.com>
2015-02-16 04:00:29 +0000

    gofmt.

--- golang/feeds/feeds.go
+++ golang/feeds/feeds.go
@@ -10,20 +10,20 @@ func main() {
 	t := time.Now()
 
 	feed := &feeds.Feed{
-		Title: "Test feed.",
-		Link: &feeds.Link{Href:"http://oyasirazu.dip.jp/"},
-		Description:"Test.",
-		Author: &feeds.Author{"mikoto2000", "mikoto2000@gmail.com"},
-		Created: t,
+		Title:       "Test feed.",
+		Link:        &feeds.Link{Href: "http://oyasirazu.dip.jp/"},
+		Description: "Test.",
+		Author:      &feeds.Author{"mikoto2000", "mikoto2000@gmail.com"},
+		Created:     t,
 	}
 
 	feed.Items = []*feeds.Item{
 		&feeds.Item{
-			Title: "Test feed item.",
-			Link: &feeds.Link{Href:"http://oyasirazu.dip.jp/item"},
-			Description:"Test.",
-			Author: &feeds.Author{"mikoto2000", "mikoto2000@gmail.com"},
-			Created: t,
+			Title:       "Test feed item.",
+			Link:        &feeds.Link{Href: "http://oyasirazu.dip.jp/item"},
+			Description: "Test.",
+			Author:      &feeds.Author{"mikoto2000", "mikoto2000@gmail.com"},
+			Created:     t,
 		},
 	}
 
