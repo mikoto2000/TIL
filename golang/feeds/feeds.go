df1410d9819b2bc66d5e1997a0be2b83a47e7a50
mikoto2000 <mikoto2000@gmail.com>
2015-02-16 03:49:55 +0000

    Implemented "feed's detail." in "golang/feeds".
    フィード詳細を記述。

--- golang/feeds/feeds.go
+++ golang/feeds/feeds.go
@@ -3,11 +3,18 @@ package main
 import (
 	"fmt"
 	"github.com/gorilla/feeds"
+	"time"
 )
 
 func main() {
+	t := time.Now()
+
 	feed := &feeds.Feed{
-		Link: &feeds.Link{},
+		Title: "Test feed.",
+		Link: &feeds.Link{Href:"http://oyasirazu.dip.jp/"},
+		Description:"Test.",
+		Author: &feeds.Author{"mikoto2000", "mikoto2000@gmail.com"},
+		Created: t,
 	}
 
 	rss, _ := feed.ToRss()
