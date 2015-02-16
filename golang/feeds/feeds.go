6bb92e4e5ac6b63f923f98ee83219625704285fc
mikoto2000 <mikoto2000@gmail.com>
2015-02-16 14:58:31 +0000

    ファイル情報を取得するように修正。

--- golang/feeds/feeds.go
+++ golang/feeds/feeds.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"github.com/gorilla/feeds"
+	"os"
+	"path/filepath"
 	"time"
 )
 
@@ -10,28 +12,55 @@ const BASE_URL = "http://oyasirazu.dip.jp/"
 const AUTHOR = "mikoto2000"
 const MAIL = "mikoto2000@gmail.com"
 
+// 第一引数で渡されたディレクトリ下を探索
+// 第二引数で渡された拡張子のファイルのみ表示する
 func main() {
-	t := time.Now()
+	if len(os.Args) != 3 {
+		os.Exit(0)
+	}
 
+	dir := os.Args[1]
+	ext := os.Args[2]
+
+	// フィードの基本情報設定
+	t := time.Now()
 	feed := &feeds.Feed{
 		Title:       "Test feed.",
 		Link:        &feeds.Link{Href: BASE_URL},
 		Description: "Test.",
 		Author:      &feeds.Author{AUTHOR, MAIL},
 		Created:     t,
+		Items:       []*feeds.Item{},
 	}
 
-	feed.Items = []*feeds.Item{
-		&feeds.Item{
-			Title:       "Test feed item.",
-			Link:        &feeds.Link{Href: BASE_URL + "item"},
-			Description: "Test.",
-			Author:      &feeds.Author{AUTHOR, MAIL},
-			Created:     t,
-		},
-	}
+	// 指定ディレクトリ下を探索しつつ
+	// フィードを組み立てる。
+	err := filepath.Walk(dir,
+		func(path string, info os.FileInfo, err error) error {
+			if info.IsDir() {
+				return nil
+			}
+
+			if filepath.Ext(path) == "."+ext {
+				rel, _ := filepath.Rel(dir, path)
+
+				feed.Items = append(feed.Items, &feeds.Item{
+					Title:       info.Name(),
+					Link:        &feeds.Link{Href: BASE_URL + rel},
+					Description: info.Name(),
+					Author:      &feeds.Author{AUTHOR, MAIL},
+					Created:     info.ModTime(),
+				})
+			}
+
+			return nil
+		})
 
 	rss, _ := feed.ToRss()
 
 	fmt.Println(rss)
+
+	if err != nil {
+		fmt.Println(1, err)
+	}
 }
