4aece3a726e24876c5002b1f7f96c81bb35428aa
mikoto2000 <mikoto2000@gmail.com>
2015-02-16 13:38:13 +0000

    定数化しやすいところを定数化した。

--- golang/feeds/feeds.go
+++ golang/feeds/feeds.go
@@ -6,23 +6,27 @@ import (
 	"time"
 )
 
+const BASE_URL = "http://oyasirazu.dip.jp/"
+const AUTHOR = "mikoto2000"
+const MAIL = "mikoto2000@gmail.com"
+
 func main() {
 	t := time.Now()
 
 	feed := &feeds.Feed{
 		Title:       "Test feed.",
-		Link:        &feeds.Link{Href: "http://oyasirazu.dip.jp/"},
+		Link:        &feeds.Link{Href: BASE_URL},
 		Description: "Test.",
-		Author:      &feeds.Author{"mikoto2000", "mikoto2000@gmail.com"},
+		Author:      &feeds.Author{AUTHOR, MAIL},
 		Created:     t,
 	}
 
 	feed.Items = []*feeds.Item{
 		&feeds.Item{
 			Title:       "Test feed item.",
-			Link:        &feeds.Link{Href: "http://oyasirazu.dip.jp/item"},
+			Link:        &feeds.Link{Href: BASE_URL + "item"},
 			Description: "Test.",
-			Author:      &feeds.Author{"mikoto2000", "mikoto2000@gmail.com"},
+			Author:      &feeds.Author{AUTHOR, MAIL},
 			Created:     t,
 		},
 	}
