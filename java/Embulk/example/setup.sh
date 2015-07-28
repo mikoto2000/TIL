4d20976bdbd82a81dbf8b69b186d7cde06cacdf8
mikoto2000 <mikoto2000@gmail.com>
2015-07-28 21:57:37 +0900

    Added embulk example.

---
+++ java/Embulk/example/setup.sh
@@ -0,0 +1,11 @@
+if [ -e "../bin/embulk" ]; then
+    echo "embulk already exists."
+else
+    curl --create-dirs -o ./bin/embulk -L "http://dl.embulk.org/embulk-latest.jar"
+    chmod +x ./bin/embulk
+    embulk example ./try1
+    embulk guess   ./try1/example.yml -o config.yml
+    embulk preview config.yml
+    embulk run     config.yml
+    export PATH="`pwd`/../bin:$PATH"
+fi
