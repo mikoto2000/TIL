eebc077bf09ff72d5abb90a6f96e3e4f6a5be3b2
mikoto2000 <mikoto2000@gmail.com>
2015-08-26 21:50:01 +0900

    Added logback file rotation sample in 'Log/Logback/fileRolling'.

---
+++ java/Log/Logback/fileRolling/src/main/java/jp/dip/oyasirazu/study/log/logback/Main.java
@@ -0,0 +1,16 @@
+package jp.dip.oyasirazu.study.log.logback;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+    public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger(Main.class);
+
+        logger.info("test : {}", System.getProperty("logback.configurationFile", "null"));
+        logger.info("info message : {}", "TESTING.");
+        logger.debug("debug message : {}", "TESTING.");
+        logger.warn("warn message : {}", "TESTING.");
+        logger.error("error message : {}", "TESTING.");
+    }
+}
