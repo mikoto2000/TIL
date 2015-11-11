64f81d0d56ff32f1e5117c5d2562dd098c4fbf68
mikoto2000 <mikoto2000@gmail.com>
2015-11-12 01:34:20 +0900

    Added JavaFX in 'java'.

---
+++ java/JavaFX/src/main/java/jp/dip/oyasirazu/study/javafx/HelloWorld.java
@@ -0,0 +1,24 @@
+package jp.dip.oyasirazu.study.javafx;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
+import javafx.scene.control.Label;
+
+public class HelloWorld extends Application {
+    public static void main(String[] args) {
+        Application.launch(HelloWorld.class, args);
+    }
+
+    @Override
+    public void start(Stage stage) {
+        HBox hbox = new HBox();
+        Scene scene = new Scene(hbox);
+        hbox.getChildren().add(new Label("Hello, World"));
+
+        stage.setScene(scene);
+        stage.setTitle("Hello");
+        stage.show();
+    }
+}
