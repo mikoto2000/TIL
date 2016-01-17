a28c1bb520c30906db0a91ed91ad681f8179cb40
mikoto2000 <mikoto2000@gmail.com>
2016-01-18 01:01:01 +0900

    Added TabPane study.

---
+++ java/JavaFX/src/main/java/jp/dip/oyasirazu/study/javafx/s07_TabPane/TabPaneController.java
@@ -0,0 +1,19 @@
+package jp.dip.oyasirazu.study.javafx;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+public class TabPaneController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+    }
+}
