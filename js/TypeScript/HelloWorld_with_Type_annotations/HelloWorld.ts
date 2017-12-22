2a4c0a2cf2d004da6fdaa9a69c70553eceef45ba
mikoto2000 <mikoto2000@gmail.com>
2017-12-23 01:03:16 +0900

    Added 'TypeScript/HelloWorld_with_Type_annotation'.

---
+++ js/TypeScript/HelloWorld_with_Type_annotations/HelloWorld.ts
@@ -0,0 +1,11 @@
+function greeter(person: string) {
+    return "Hello, " + person + "!";
+}
+
+let user = "mikoto2000";
+
+// error
+// let user = 123;
+
+console.log(greeter(user));
+
---
+++ js/TypeScript/HelloWorld_with_Type_annotations/README.md
@@ -0,0 +1,5 @@
+> npm install -g typescript
+> tsc ./HelloWorld.ts
+> node ./HelloWorld.js
+Hello, mikoto2000!
+
