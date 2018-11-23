499707e4ab1ae586f443772379698599de847427
mikoto2000 <mikoto2000@gmail.com>
2018-11-24 08:19:01 +0900

    Added route in `js/nodejs/WebServer/Express/add-route`.

---
+++ js/nodejs/WebServer/Express/add-route/AddRoute/routes/categories.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a categories resource');
+});
+
+module.exports = router;
+
