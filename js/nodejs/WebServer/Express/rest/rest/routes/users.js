e2ff48ae15ea56053e0ccab4eed5ece1e18a4c00
mikoto2000 <mikoto2000@gmail.com>
2018-11-24 09:07:48 +0900

    Modified responce to REST in `js/nodejs/WebServer/Express/rest`.

--- js/nodejs/WebServer/Express/rest/rest/routes/users.js
+++ js/nodejs/WebServer/Express/rest/rest/routes/users.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  let users = [
+      {"id": 1, "name": "User01"},
+      {"id": 2, "name": "User02"},
+      {"id": 3, "name": "User03"},
+      {"id": 4, "name": "User04"},
+  ];
+  res.send(users);
 });
 
 module.exports = router;
