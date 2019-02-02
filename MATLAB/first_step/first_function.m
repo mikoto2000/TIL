64189ee5db27b038225f02558716334a58d47402
mikoto2000 <mikoto2000@gmail.com>
2019-02-02 16:49:42 +0900

    関数の作成・使用方法を追加。

---
+++ MATLAB/first_step/first_function.m
@@ -0,0 +1,9 @@
+function [result1, result2] = first_function()
+% •¶š—ñ 'hello' ‚Æ•¶š—ñ 'world' ‚ğ•Ô‹p‚µ‚Ü‚·B
+%
+% Example:
+%     [hello, world] = first_function();
+result1 = 'hello';
+result2 = 'world';
+end
+
