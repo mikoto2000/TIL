bbcaa2adec6de3a923d48591c07a8f6e5e9d6182
mikoto2000 <mikoto2000@gmail.com>
2019-05-01 17:29:06 +0900

    SnakeYAML を使用した YAML パースの例を追加 in `Java/Yaml/SnakeYaml/firstStep`.

--- java/Yaml/SnakeYaml/firstStep/src/main/java/jp/dip/oyasirazu/study/yaml/firststep/App.java
+++ java/Yaml/SnakeYaml/firstStep/src/main/java/jp/dip/oyasirazu/study/yaml/firststep/App.java
@@ -3,12 +3,43 @@
  */
 package jp.dip.oyasirazu.study.yaml.firststep;
 
+import java.util.Map;
+
+import org.yaml.snakeyaml.Yaml;
+
 public class App {
-    public String getGreeting() {
-        return "Hello world.";
+    public static String getYamlString() {
+        return "root:\n"
+            + "  child1:\n"
+            + "    key: 'key1'\n"
+            + "    value: 'value1'\n"
+            + "  child2:\n"
+            + "    key: 'key2'\n"
+            + "    value: 'value2'\n";
     }
 
+    @SuppressWarnings("unchecked")
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        String yamlString = App.getYamlString();
+
+        Yaml yaml = new Yaml();
+        var test = yaml.<Map<String, Object>>load(yamlString);
+
+        // java.util.LinkedHashMap
+        System.out.printf("test.getClass(): %s\n", test.getClass());
+
+        // {root={child1={key=key1, value=value1}, child2={key=key2, value=value2}}}
+        System.out.printf("test: %s\n", test);
+
+        // {child1={key=key1, value=value1}, child2={key=key2, value=value2}}
+        var root = (Map<String, Object>)test.get("root");
+        System.out.printf("root: %s\n", root);
+
+        // {key=key1, value=value1}
+        var child1 = (Map<String, Object>)root.get("child1");
+        System.out.printf("child1: %s\n", child1);
+
+        // value1
+        System.out.printf("value: %s\n", child1.get("value"));
     }
 }
