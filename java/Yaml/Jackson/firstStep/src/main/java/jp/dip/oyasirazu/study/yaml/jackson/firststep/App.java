7cac68d373f4eb66506c2c24ba5a35fa50a8f79a
mikoto2000 <mikoto2000@gmail.com>
2019-05-02 01:19:28 +0900

    Jackson を使用した YAML パースの例を追加 in `Java/Yaml/Jackson/firstStep`.

--- java/Yaml/Jackson/firstStep/src/main/java/jp/dip/oyasirazu/study/yaml/jackson/firststep/App.java
+++ java/Yaml/Jackson/firstStep/src/main/java/jp/dip/oyasirazu/study/yaml/jackson/firststep/App.java
@@ -3,12 +3,43 @@
  */
 package jp.dip.oyasirazu.study.yaml.jackson.firststep;
 
+import java.io.IOException;
+import java.util.Map;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+
 public class App {
-    public String getGreeting() {
-        return "Hello world.";
+    public static String getYamlString() {
+        return "root:\n"
+            + "  child1:\n"
+            + "    key: 'key1'\n"
+            + "    value: 'value1'\n"
+            + "  child2:\n"
+            + "    key: 'key2'\n"
+            + "    value: 'value2'\n";
     }
 
-    public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+    @SuppressWarnings("unchecked")
+    public static void main(String[] args) throws IOException {
+        var mapper = new ObjectMapper(new YAMLFactory());
+        var test = mapper.readValue(App.getYamlString(), Map.class);
+
+        // java.util.LinkedHashMap
+        System.out.printf("test.getClass(): %s\n", test.getClass());
+
+        // {root={child1={key=key1, value=value1}, child2={key=key2, value=value2}}}
+        System.out.printf("test: %s\n", test);
+
+        // {child1={key=key1, value=value1}, child2={key=key2, value=value2}}
+        var root = (Map<String, Object>)test.get("root");
+        System.out.printf("root: %s\n", root);
+
+        // {key=key1, value=value1}
+        var child1 = (Map<String, Object>)root.get("child1");
+        System.out.printf("child1: %s\n", child1);
+
+        // value1
+        System.out.printf("value: %s\n", child1.get("value"));
     }
 }
