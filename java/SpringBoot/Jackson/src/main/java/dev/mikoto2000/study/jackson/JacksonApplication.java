50791b51e56671d3ffb298556f52f6396b28fe7e
mikoto2000 <mikoto2000@gmail.com>
2020-10-05 06:54:41 +0900

    Added `java/SpringBoot/Jackson`.

---
+++ java/SpringBoot/Jackson/src/main/java/dev/mikoto2000/study/jackson/JacksonApplication.java
@@ -0,0 +1,13 @@
+package dev.mikoto2000.study.jackson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JacksonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JacksonApplication.class, args);
+	}
+
+}
