d109f3ce7d9d3664534fe803c3593b14f93d96b3
mikoto2000 <mikoto2000@gmail.com>
2021-06-22 05:56:21 +0900

    Added java/SpringBoot/Keycloak

---
+++ java/SpringBoot/Keycloak/gettingstarted/src/test/java/dev/mikoto2000/study/springboot/keycloak/gettingstarted/GettingstartedApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mikoto2000.study.springboot.keycloak.gettingstarted;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GettingstartedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
