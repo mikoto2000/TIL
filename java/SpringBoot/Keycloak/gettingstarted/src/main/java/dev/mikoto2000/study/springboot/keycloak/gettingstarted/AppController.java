98f4777b8e64e54d626836795b8a766cff632d2b
mikoto2000 <mikoto2000@gmail.com>
2021-06-22 06:28:05 +0900

    Added controller and template.

---
+++ java/SpringBoot/Keycloak/gettingstarted/src/main/java/dev/mikoto2000/study/springboot/keycloak/gettingstarted/AppController.java
@@ -0,0 +1,28 @@
+package dev.mikoto2000.study.springboot.keycloak.gettingstarted;
+
+import java.security.Principal;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+/**
+ * AppController
+ */
+@Controller
+public class AppController {
+    @GetMapping(path = "/")
+    public String index(Principal principal, Model model) {
+        model.addAttribute("username", principal.getName());
+
+        return "index";
+    }
+
+    @GetMapping(path = "/users/{name}")
+    public String customers(@PathVariable("name") String name, Principal principal, Model model) {
+
+        model.addAttribute("username", principal.getName());
+        return "userpage";
+    }
+}
