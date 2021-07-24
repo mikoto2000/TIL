d5e80eade587ee23acfdaffd804ff812d658dc71
mikoto2000 <mikoto2000@gmail.com>
2021-07-25 07:28:32 +0900

    Added java/SpringBoot/FileUpload/ajax.

---
+++ java/SpringBoot/FileUpload/ajax/src/main/java/dev/mikoto2000/study/springboot/file/ajax/AjaxApplication.java
@@ -0,0 +1,13 @@
+package dev.mikoto2000.study.springboot.file.ajax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjaxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AjaxApplication.class, args);
+	}
+
+}
