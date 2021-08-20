51f59def9812c600e7f3c4b5df46ca7d2c55a096
mikoto2000 <mikoto2000@gmail.com>
2021-08-20 09:20:19 +0900

    MQTT クライアントを実装。
    See: https://vertx.io/docs/vertx-mqtt/java/

--- java/Vertx/mqtt/src/main/java/dev/mikoto2000/study/vertx/mqtt/MainVerticle.java
+++ java/Vertx/mqtt/src/main/java/dev/mikoto2000/study/vertx/mqtt/MainVerticle.java
@@ -1,23 +1,33 @@
 package dev.mikoto2000.study.vertx.mqtt;
 
+import java.time.LocalDateTime;
+
+import io.netty.handler.codec.mqtt.MqttQoS;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.mqtt.MqttClient;
 
 public class MainVerticle extends AbstractVerticle {
 
   @Override
   public void start(Promise<Void> startPromise) throws Exception {
-    vertx.createHttpServer().requestHandler(req -> {
-      req.response()
-        .putHeader("content-type", "text/plain")
-        .end("Hello from Vert.x!");
-    }).listen(8888, http -> {
-      if (http.succeeded()) {
-        startPromise.complete();
-        System.out.println("HTTP server started on port 8888");
-      } else {
-        startPromise.fail(http.cause());
-      }
+    MqttClient client = MqttClient.create(vertx);
+
+    client.connect(1883, "localhost", s -> {
+        System.out.println("connected localhost:1883.");
+
+    client.publishHandler(topic -> {
+        System.out.println(String.format("{ topic: %s, payload: %s }", topic.topicName(), topic.payload().toString()));
+    }).subscribe("temperature", 2);
+
+    vertx.setPeriodic(1000, id -> {
+      client.publish("temperature",
+        Buffer.buffer(LocalDateTime.now().toString()),
+        MqttQoS.AT_LEAST_ONCE,
+        false,
+        false);
+      });
     });
   }
 }
