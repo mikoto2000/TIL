3ea499116ed67dd29dca42767fe181d8a1ff8665
mikoto2000 <mikoto2000@gmail.com>
2021-10-24 12:27:46 +0900

    tmp

--- java/sftp/sshj/firststep/src/main/java/dev/mikoto2000/study/sftp/sshj/firststep/FirststepApplication.java
+++ java/sftp/sshj/firststep/src/main/java/dev/mikoto2000/study/sftp/sshj/firststep/FirststepApplication.java
@@ -2,12 +2,22 @@ package dev.mikoto2000.study.sftp.sshj.firststep;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class FirststepApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FirststepApplication.class, args);
-	}
-
+    public static void main(final String[] args) {
+        try (ConfigurableApplicationContext ctx = SpringApplication.run(FirststepApplication.class, args)) {
+            final FirststepApplication app = ctx.getBean(FirststepApplication.class);
+            app.run(args);
+        } catch (final Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void run(final String... args) throws Exception {
+        System.out.println("処理開始");
+        //アプリの処理
+        System.out.println("処理終了");
+    }
 }
