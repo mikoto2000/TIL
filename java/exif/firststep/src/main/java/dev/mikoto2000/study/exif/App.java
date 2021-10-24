bc825a95b374340e6e9e6bebd45e9e45c1608a94
mikoto2000 <mikoto2000@gmail.com>
2021-10-25 04:35:34 +0900

    Added sample source in `java/exif/firststep`.

--- java/exif/firststep/src/main/java/dev/mikoto2000/study/exif/App.java
+++ java/exif/firststep/src/main/java/dev/mikoto2000/study/exif/App.java
@@ -1,13 +1,102 @@
 package dev.mikoto2000.study.exif;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import org.apache.commons.imaging.ImageReadException;
+import org.apache.commons.imaging.ImageWriteException;
+import org.apache.commons.imaging.Imaging;
+import org.apache.commons.imaging.common.ImageMetadata;
+import org.apache.commons.imaging.common.RationalNumber;
+import org.apache.commons.imaging.formats.jpeg.JpegImageMetadata;
+import org.apache.commons.imaging.formats.jpeg.exif.ExifRewriter;
+import org.apache.commons.imaging.formats.tiff.TiffImageMetadata;
+import org.apache.commons.imaging.formats.tiff.constants.ExifTagConstants;
+import org.apache.commons.imaging.formats.tiff.constants.GpsTagConstants ;
+import org.apache.commons.imaging.formats.tiff.constants.TiffConstants ;
+import org.apache.commons.imaging.formats.tiff.constants.TiffDirectoryConstants ;
+import org.apache.commons.imaging.formats.tiff.fieldtypes.FieldType;
+import org.apache.commons.imaging.formats.tiff.write.TiffOutputDirectory;
+import org.apache.commons.imaging.formats.tiff.write.TiffOutputField;
+import org.apache.commons.imaging.formats.tiff.write.TiffOutputSet;
+import org.apache.commons.io.FileUtils;
+
 /**
- * Hello world!
+ * GPS 情報追加サンプル。
  *
+ * See: https://github.com/apache/commons-imaging/blob/master/src/test/java/org/apache/commons/imaging/examples/WriteExifMetadataExample.java
+ *      https://www.javatips.net/api/org.apache.commons.imaging.formats.tiff.constants.gpstagconstants.gps.tag.gps.img.direction
+ *      https://www.cipa.jp/std/documents/j/DC-008-2010_J.pdf
+ *
+ * 作成した GPS 情報は、以下サイトで確認できる。
+ *      http://exif-check.org/
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main( String[] args ) throws Exception {
+        final File org = new File("./test.jpg");
+        final File dst = new File("./test_dst.jpg");
+
+        setExifGPSTag(org, dst);
+
         System.out.println( "Hello World!" );
+    }
+
+    public static void setExifGPSTag(final File jpegImageFile, final File dst) throws IOException,
+            ImageReadException, ImageWriteException {
+        try (FileOutputStream fos = new FileOutputStream(dst);
+                OutputStream os = new BufferedOutputStream(fos)) {
+            TiffOutputSet outputSet = null;
+
+            // note that metadata might be null if no metadata is found.
+            final ImageMetadata metadata = Imaging.getMetadata(jpegImageFile);
+            final JpegImageMetadata jpegMetadata = (JpegImageMetadata) metadata;
+            if (null != jpegMetadata) {
+                // note that exif might be null if no Exif metadata is found.
+                final TiffImageMetadata exif = jpegMetadata.getExif();
+
+                if (null != exif) {
+                    // TiffImageMetadata class is immutable (read-only).
+                    // TiffOutputSet class represents the Exif data to write.
+                    //
+                    // Usually, we want to update existing Exif metadata by
+                    // changing
+                    // the values of a few fields, or adding a field.
+                    // In these cases, it is easiest to use getOutputSet() to
+                    // start with a "copy" of the fields read from the image.
+                    outputSet = exif.getOutputSet();
+                }
+            }
+
+            // if file does not contain any exif metadata, we create an empty
+            // set of exif metadata. Otherwise, we keep all of the other
+            // existing tags.
+            if (null == outputSet) {
+                outputSet = new TiffOutputSet();
+            }
+
+            {
+                // Example of how to add/update GPS info to output set.
+
+                // New York City
+                final double longitude = -74.0; // 74 degrees W (in Degrees East)
+                final double latitude = 40 + 43 / 60.0; // 40 degrees N (in Degrees
+                // North)
+
+                outputSet.setGPSInDegrees(longitude, latitude);
+            }
+
+            {
+                TiffOutputDirectory gpsDirectory = outputSet.getOrCreateGPSDirectory();;
+                gpsDirectory.add(GpsTagConstants.GPS_TAG_GPS_ALTITUDE, RationalNumber.valueOf(1000));
+                gpsDirectory.add(GpsTagConstants.GPS_TAG_GPS_IMG_DIRECTION_REF, GpsTagConstants.GPS_TAG_GPS_IMG_DIRECTION_REF_VALUE_MAGNETIC_NORTH );
+                gpsDirectory.add(GpsTagConstants.GPS_TAG_GPS_IMG_DIRECTION, RationalNumber.valueOf(180.0));
+            }
+
+            new ExifRewriter().updateExifMetadataLossless(jpegImageFile, os,
+                    outputSet);
+        }
     }
 }
