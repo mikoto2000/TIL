cea24c1ce5f6ecc6cf539839174de0e7da2fcb2f
mikoto2000 <mikoto2000@gmail.com>
2021-10-25 03:19:19 +0900

    Added `java/exif/firststep`.

---
+++ java/exif/firststep/src/main/java/dev/mikoto2000/study/exif/App.java
@@ -0,0 +1,13 @@
+package dev.mikoto2000.study.exif;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
