1810d422a780780166238e312823d7f7190d47ca
mikoto2000 <mikoto2000@gmail.com>
2022-01-29 04:12:37 +0900

    Added `csharp/misc/PriorityQueue`.

---
+++ csharp/misc/PriorityQueue/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+
+class PriorityQueueExample
+{
+
+    static void Main(string[] args)
+    {
+        // TryDequeue を利用した値取得
+        {
+            var priorityQueue1 = new PriorityQueue<string, int>();
+
+            priorityQueue1.Enqueue("pri2, ddd.", 2);
+            priorityQueue1.Enqueue("pri2, ccc.", 2);
+            priorityQueue1.Enqueue("pri1, aaa.", 1);
+            priorityQueue1.Enqueue("pri1, bbb.", 1);
+
+            // キューに詰めたものを順番に取り出す
+            // 優先度の高いものを FIFO で取得した後、
+            // 優先度の低いものが FIFO で取得できる。
+            while (priorityQueue1.TryDequeue(out string item, out int priority))
+            {
+                Console.WriteLine($"Popped Item : {item}. Priority Was : {priority}");
+            }
+        }
+
+        // Dequeue を利用した値取得
+        {
+            var priorityQueue2 = new PriorityQueue<string, int>();
+
+            priorityQueue2.Enqueue("pri2, ddd.", 2);
+            priorityQueue2.Enqueue("pri2, ccc.", 2);
+            priorityQueue2.Enqueue("pri1, aaa.", 1);
+            priorityQueue2.Enqueue("pri1, bbb.", 1);
+
+            try
+            {
+                // キューに詰めたものを順番に取り出す
+                // 優先度の高いものを FIFO で取得した後、
+                // 優先度の低いものが FIFO で取得できる。
+                while (true)
+                {
+                    string v = priorityQueue2.Dequeue();
+                    Console.WriteLine(v);
+                }
+            }
+            catch (InvalidOperationException e)
+            {
+                // キューが空の場合、 InvalidOperationException が発生する
+                Console.WriteLine(e);
+            }
+        }
+
+    }
+}
+
---
+++ csharp/misc/PriorityQueue/README.md
@@ -0,0 +1,27 @@
+# PriorityQueue
+
+優先度付きキューの使用例。
+
+## プロジェクトの準備
+
+```sh
+dotnet new console
+```
+
+## Build
+
+```sh
+dotnet build
+```
+
+## Run
+
+```sh
+dotnet run
+```
+
+## 参考資料
+
+- [PriorityQueue<TElement,TPriority> Class (System.Collections.Generic) | Microsoft Docs](https://docs.microsoft.com/en-us/dotnet/api/system.collections.generic.priorityqueue-2?view=net-6.0)
+- [例外と例外処理 | Microsoft Docs](https://docs.microsoft.com/ja-jp/dotnet/csharp/fundamentals/exceptions/)
+
