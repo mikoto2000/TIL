8a28a4408e1408cce20a3c6b03b80a0b16da6706
mikoto2000 <mikoto2000@gmail.com>
2022-03-11 13:05:38 +0900

    Added blocking queue test and implementation in `js/nodejs/BlockingQueue`.

--- js/nodejs/BlockingQueue/BlockingQueue.test.js
+++ js/nodejs/BlockingQueue/BlockingQueue.test.js
@@ -1,13 +1,13 @@
 import { BlockingQueue } from './BlockingQueue.js';
 
-test('Simple push and pop.', () => {
+test('Simple push and pop.', async () => {
     const first = 1;
     const second = 2;
     const third = 3;
 
     const bq = new BlockingQueue();
 
-    var size = bq.push(first);
+    let size = bq.push(first);
     expect(size).toBe(1);
     expect(bq.size()).toBe(1);
 
@@ -19,15 +19,52 @@ test('Simple push and pop.', () => {
     expect(size).toBe(3);
     expect(bq.size()).toBe(3);
 
-    expect(bq.shift()).toBe(1);
+    let e = await bq.shift();
+    expect(e).toBe(first);
     expect(bq.size()).toBe(2);
 
-    expect(bq.shift()).toBe(2);
+    e = await bq.shift();
+    expect(e).toBe(second);
     expect(bq.size()).toBe(1);
 
-    expect(bq.shift()).toBe(3);
+    e = await bq.shift();
+    expect(e).toBe(third);
     expect(bq.size()).toBe(0);
 
 });
 
+test('Blocking push and pop.', done => {
 
+    blockingPushAndPop(done);
+
+});
+
+async function blockingPushAndPop(done) {
+    const first = 1;
+    const second = 2;
+    const third = 3;
+
+    const bq = new BlockingQueue();
+
+    setTimeout( async () => {
+        bq.push(first);
+        await new Promise(_ => setTimeout(_, 100));
+        bq.push(second);
+        await new Promise(_ => setTimeout(_, 100));
+        bq.push(third);
+    }, 100);
+
+    let e = await bq.shift();
+    expect(e).toBe(first);
+    expect(bq.size()).toBe(0);
+
+    e = await bq.shift();
+    expect(e).toBe(second);
+    expect(bq.size()).toBe(0);
+
+    e = await bq.shift();
+    expect(e).toBe(third);
+    expect(bq.size()).toBe(0);
+
+    done();
+}
