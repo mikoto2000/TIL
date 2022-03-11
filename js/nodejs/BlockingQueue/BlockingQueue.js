8b9a93f771746c3fb9dfa8c020f8310945fd190c
mikoto2000 <mikoto2000@gmail.com>
2022-03-11 13:05:38 +0900

    Added blocking queue test and implementation in `js/nodejs/BlockingQueue`.

--- js/nodejs/BlockingQueue/BlockingQueue.js
+++ js/nodejs/BlockingQueue/BlockingQueue.js
@@ -1,18 +1,47 @@
 export class BlockingQueue {
     constructor() {
-        this.queue = [];
+        this.resolverQueue = [];
+        this.promiseQueue = [];
     }
 
     push(data) {
-        return this.queue.push(data);
+        // リゾルバがひとつもない時には、
+        // 新しいプロミス(とそれ用のリゾルバ)を追加する
+        if (this.resolverQueue.length <= 0) {
+            this.registerPromise();
+        }
+
+        // リゾルバキューの先頭を使って、
+        // プロミスに値を格納
+        this.resolverQueue.shift()(data);
+
+        // 残り要素数を返却
+        return this.promiseQueue.length - this.resolverQueue.length;
     }
 
     shift() {
-        return this.queue.shift();
+        // プロミスがひとつもない時には、
+        // 新しいプロミス(とそれ用のリゾルバ)を追加する
+        if (this.promiseQueue.length <= 0) {
+            this.registerPromise();
+        }
+
+        // プロミスキューの先頭を返却
+        return this.promiseQueue.shift();
+    }
+
+    /**
+     * プロミスキューにプロミス追加し、
+     * そのプロミス用のリゾルバをリゾルバキューへ格納する
+     */
+    registerPromise() {
+        this.promiseQueue.push(new Promise(resolve => {
+            this.resolverQueue.push(resolve);
+        }));
     }
 
     size() {
-        return this.queue.length;
+        return this.promiseQueue.length;
     }
 }
 
