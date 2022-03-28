e5455b0e092bfea1390e54c42de43c88055648b8
mikoto2000 <mikoto2000@gmail.com>
2022-03-28 20:25:54 +0900

    Added `js/monaco-editor/yaml`.

---
+++ js/monaco-editor/yaml/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
+
+const distPath = path.resolve(__dirname, 'dist');
+
+module.exports = {
+    mode: 'development',
+    entry: './index.js',
+    output: {
+        path: distPath,
+        filename: '[name].bundle.js'
+    },
+    devServer: {
+        static: distPath,
+        open: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.ttf$/,
+                use: ['file-loader']
+            }
+        ]
+    },
+    plugins: [
+        new MonacoWebpackPlugin({
+            languages: ['typescript', 'javascript', 'css']
+        })
+    ]
+};
