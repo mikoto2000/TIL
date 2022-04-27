09110433ed8e8b77b38a72fdd28a55de840c7453
mikoto2000 <mikoto2000@gmail.com>
2022-04-27 20:12:07 +0900

    'X' と 'O' を交互にする処理を追加。

--- js/React/firststep/src/index.js
+++ js/React/firststep/src/index.js
@@ -16,14 +16,18 @@ class Board extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      squares: Array(9).fill(null)
+      squares: Array(9).fill(null),
+      xIsNext: true
     };
   }
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = 'O';
-    this.setState({squares: squares});
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !(this.state.xIsNext)
+    });
   }
 
   renderSquare(i) {
@@ -36,7 +40,7 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
 
     return (
       <div>
