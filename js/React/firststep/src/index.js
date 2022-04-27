03f05be98f4b4fc65001071cda591781190b8be2
mikoto2000 <mikoto2000@gmail.com>
2022-04-27 20:04:53 +0900

    「State のリフトアップ」まで実装。

--- js/React/firststep/src/index.js
+++ js/React/firststep/src/index.js
@@ -3,27 +3,38 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null
-    };
-  }
-
   render() {
     return (
       <button className="square"
-        onClick={() => { this.setState({ value: 'X' }) }}
-        >
-        {this.state.value}
+        onClick={() => { this.props.onClick() }}
+      >
+        {this.props.value}
       </button>
     );
   }
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null)
+    };
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'O';
+    this.setState({squares: squares});
+  }
+
   renderSquare(i) {
-    return <Square value={i} />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => { this.handleClick(i) }}
+      />
+    );
   }
 
   render() {
