f85eee7302ed71593af26b57cb9c54f98f07f22d
mikoto2000 <mikoto2000@gmail.com>
2022-05-26 17:22:20 +0900

    Added firststep project in Langium.
    `yo langium` を実行した直後。

---
+++ Langium/firststep/firststep/src/cli/index.ts
@@ -0,0 +1,36 @@
+import colors from 'colors';
+import { Command } from 'commander';
+import { Model } from '../language-server/generated/ast';
+import { FirststepLanguageMetaData } from '../language-server/generated/module';
+import { createFirststepServices } from '../language-server/firststep-module';
+import { extractAstNode } from './cli-util';
+import { generateJavaScript } from './generator';
+
+export const generateAction = async (fileName: string, opts: GenerateOptions): Promise<void> => {
+    const services = createFirststepServices().Firststep;
+    const model = await extractAstNode<Model>(fileName, services);
+    const generatedFilePath = generateJavaScript(model, fileName, opts.destination);
+    console.log(colors.green(`JavaScript code generated successfully: ${generatedFilePath}`));
+};
+
+export type GenerateOptions = {
+    destination?: string;
+}
+
+export default function(): void {
+    const program = new Command();
+
+    program
+        // eslint-disable-next-line @typescript-eslint/no-var-requires
+        .version(require('../../package.json').version);
+
+    const fileExtensions = FirststepLanguageMetaData.fileExtensions.join(', ');
+    program
+        .command('generate')
+        .argument('<file>', `source file (possible file extensions: ${fileExtensions})`)
+        .option('-d, --destination <dir>', 'destination directory of generating')
+        .description('generates JavaScript code that prints "Hello, {name}!" for each greeting in a source file')
+        .action(generateAction);
+
+    program.parse(process.argv);
+}
