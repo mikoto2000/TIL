2d7dad6a3fe3d45154b42457cb1f21bfb2596e1c
mikoto2000 <mikoto2000@gmail.com>
2022-06-13 15:39:11 +0900

    Added `Langium/arithmetics`.

---
+++ Langium/arithmetics/src/language-server/arithmetics-validator.ts
@@ -0,0 +1,38 @@
+import { ValidationAcceptor, ValidationCheck, ValidationRegistry } from 'langium';
+import { ArithmeticsAstType, Person } from './generated/ast';
+import type { ArithmeticsServices } from './arithmetics-module';
+
+/**
+ * Map AST node types to validation checks.
+ */
+type ArithmeticsChecks = { [type in ArithmeticsAstType]?: ValidationCheck | ValidationCheck[] }
+
+/**
+ * Registry for validation checks.
+ */
+export class ArithmeticsValidationRegistry extends ValidationRegistry {
+    constructor(services: ArithmeticsServices) {
+        super(services);
+        const validator = services.validation.ArithmeticsValidator;
+        const checks: ArithmeticsChecks = {
+            Person: validator.checkPersonStartsWithCapital
+        };
+        this.register(checks, validator);
+    }
+}
+
+/**
+ * Implementation of custom validations.
+ */
+export class ArithmeticsValidator {
+
+    checkPersonStartsWithCapital(person: Person, accept: ValidationAcceptor): void {
+        if (person.name) {
+            const firstChar = person.name.substring(0, 1);
+            if (firstChar.toUpperCase() !== firstChar) {
+                accept('warning', 'Person name should start with a capital.', { node: person, property: 'name' });
+            }
+        }
+    }
+
+}
