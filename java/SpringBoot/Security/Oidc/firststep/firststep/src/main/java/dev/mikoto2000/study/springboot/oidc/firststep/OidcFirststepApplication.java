7faca2de30d26adb10e076338b2b0383a88f7743
mikoto2000 <mikoto2000@gmail.com>
2023-01-21 19:40:24 +0900

    Added project template `java/SpringBoot/Security/Oidc/firststep`.

---
+++ java/SpringBoot/Security/Oidc/firststep/firststep/src/main/java/dev/mikoto2000/study/springboot/oidc/firststep/OidcFirststepApplication.java
@@ -0,0 +1,13 @@
+package dev.mikoto2000.study.springboot.oidc.firststep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OidcFirststepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OidcFirststepApplication.class, args);
+	}
+
+}
---
+++ java/SpringBoot/Security/Oidc/firststep/firststep/src/test/java/dev/mikoto2000/study/springboot/oidc/firststep/OidcFirststepApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mikoto2000.study.springboot.oidc.firststep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OidcFirststepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
