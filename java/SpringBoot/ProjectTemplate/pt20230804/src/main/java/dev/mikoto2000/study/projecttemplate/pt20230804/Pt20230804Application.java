e2d10dcf21fc2a4a54f32aa04c4609618059060f
mikoto2000 <mikoto2000@gmail.com>
2023-08-04 23:54:51 +0900

    Added project template 2023/8/4 ver.

---
+++ java/SpringBoot/ProjectTemplate/pt20230804/src/main/java/dev/mikoto2000/study/projecttemplate/pt20230804/Pt20230804Application.java
@@ -0,0 +1,13 @@
+package dev.mikoto2000.study.projecttemplate.pt20230804;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pt20230804Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pt20230804Application.class, args);
+	}
+
+}
---
+++ java/SpringBoot/ProjectTemplate/pt20230804/src/test/java/dev/mikoto2000/study/projecttemplate/pt20230804/Pt20230804ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mikoto2000.study.projecttemplate.pt20230804;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pt20230804ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
