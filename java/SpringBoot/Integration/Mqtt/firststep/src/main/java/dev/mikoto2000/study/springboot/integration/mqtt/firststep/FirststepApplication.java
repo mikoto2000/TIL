b1a61b459221c9ad1767a9aef29959cf0e5089de
mikoto2000 <mikoto2000@gmail.com>
2023-09-11 23:52:32 +0900

    プログラムが停止しないように修正。

--- java/SpringBoot/Integration/Mqtt/firststep/src/main/java/dev/mikoto2000/study/springboot/integration/mqtt/firststep/FirststepApplication.java
+++ java/SpringBoot/Integration/Mqtt/firststep/src/main/java/dev/mikoto2000/study/springboot/integration/mqtt/firststep/FirststepApplication.java
@@ -1,13 +1,23 @@
 package dev.mikoto2000.study.springboot.integration.mqtt.firststep;
 
+import java.util.concurrent.CountDownLatch;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FirststepApplication {
 
+    private static final CountDownLatch latch = new CountDownLatch(1);
+
 	public static void main(String[] args) {
 		SpringApplication.run(FirststepApplication.class, args);
+
+		try {
+			latch.await();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
