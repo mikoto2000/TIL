d87756293e783ec7921818150e7f572afd643de6
mikoto2000 <mikoto2000@gmail.com>
2023-10-14 13:56:46 +0900

    Added `java/SpringBoot/Database/Hibernate/GenerateEntity`.

---
+++ java/SpringBoot/Database/Hibernate/GenerateEntity/src/main/java/dev/mikoto2000/study/springboot/hibernate/GenerateEntity/GenerateEntityApplication.java
@@ -0,0 +1,13 @@
+package dev.mikoto2000.study.springboot.hibernate.GenerateEntity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenerateEntityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenerateEntityApplication.class, args);
+	}
+
+}
