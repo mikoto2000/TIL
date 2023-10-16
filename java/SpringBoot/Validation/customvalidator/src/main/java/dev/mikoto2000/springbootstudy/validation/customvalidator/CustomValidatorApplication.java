cade4f32d4a389af683bbebfcfe6dde55bea9235
mikoto2000 <mikoto2000@gmail.com>
2023-10-17 08:34:55 +0900

    Added `java/SpringBoot/Validation/CustomValidator`.

---
+++ java/SpringBoot/Validation/customvalidator/src/main/java/dev/mikoto2000/springbootstudy/validation/customvalidator/CustomValidatorApplication.java
@@ -0,0 +1,13 @@
+package dev.mikoto2000.springbootstudy.validation.customvalidator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomValidatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomValidatorApplication.class, args);
+	}
+
+}
