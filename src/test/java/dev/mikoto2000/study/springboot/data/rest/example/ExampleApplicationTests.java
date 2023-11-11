51edfaff49aea888763a050f04bebec0818fa30e
mikoto2000 <mikoto2000@gmail.com>
2023-11-11 23:59:32 +0900

    Added initial project.
    https://start.spring.io/#!type=maven-project&language=java&platformVersion=3.1.5&packaging=jar&jvmVersion=17&groupId=dev.mikoto2000.study.springboot.data.rest&artifactId=example&name=Example&description=Demo%20project%20for%20Spring%20Data%20REST&packageName=dev.mikoto2000.study.springboot.data.rest.example&dependencies=data-jpa,web,postgresql,lombok,devtools

---
+++ src/test/java/dev/mikoto2000/study/springboot/data/rest/example/ExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mikoto2000.study.springboot.data.rest.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
