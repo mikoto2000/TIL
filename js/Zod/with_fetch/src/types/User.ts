7180e144dfc41c09a1b231ce187edae0577b42c8
mikoto2000 <mikoto2000@gmail.com>
2024-01-01 01:18:25 +0900

    fetch のレスポンスとして受け取る型定義を追加。

--- js/Zod/with_fetch/src/types/User.ts
+++ js/Zod/with_fetch/src/types/User.ts
@@ -1,6 +1,8 @@
 import { z } from "zod";
 
+// 静的型定義
 export type User = z.infer<typeof UserSchema>;
+export type Users = z.infer<typeof UsersSchema>;
 
 // ランタイム型の定義
 export const UserSchema = z.object({
@@ -8,4 +10,5 @@ export const UserSchema = z.object({
   lastName: z.string(),
   age: z.number(),
 });
+export const UsersSchema = z.array(UserSchema);
 
