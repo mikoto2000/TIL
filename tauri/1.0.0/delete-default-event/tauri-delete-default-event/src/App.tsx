3af2ba96c990485f43479739d922af893929b469
mikoto2000 <mikoto2000@gmail.com>
2024-06-12 04:04:30 +0000

    delete default event in `tauri/1.0.0/delete-default-event`.

--- tauri/1.0.0/delete-default-event/tauri-delete-default-event/src/App.tsx
+++ tauri/1.0.0/delete-default-event/tauri-delete-default-event/src/App.tsx
@@ -1,16 +1,10 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
 
 function App() {
   const [greetMsg, setGreetMsg] = useState("");
   const [name, setName] = useState("");
-
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
-  }
 
   return (
     <div className="container">
@@ -34,7 +28,7 @@ function App() {
         className="row"
         onSubmit={(e) => {
           e.preventDefault();
-          greet();
+          setGreetMsg(`Hello, ${name}! You've been greeted from Rust!`);
         }}
       >
         <input
