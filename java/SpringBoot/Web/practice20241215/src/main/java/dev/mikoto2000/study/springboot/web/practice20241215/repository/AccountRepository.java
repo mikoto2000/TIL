daec8bcddfd98d454d749a7a3bede72ceb19420d
mikoto2000 <mikoto2000@gmail.com>
2024-12-15 20:35:27 +0900

    Added projection for AccountRepository.

--- java/SpringBoot/Web/practice20241215/src/main/java/dev/mikoto2000/study/springboot/web/practice20241215/repository/AccountRepository.java
+++ java/SpringBoot/Web/practice20241215/src/main/java/dev/mikoto2000/study/springboot/web/practice20241215/repository/AccountRepository.java
@@ -2,12 +2,15 @@ package dev.mikoto2000.study.springboot.web.practice20241215.repository;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import dev.mikoto2000.study.springboot.web.practice20241215.entity.Account;
+import dev.mikoto2000.study.springboot.web.practice20241215.projection.DefaultAccountProjection;
 
 /**
  * AccountRepository
  */
+@RepositoryRestResource(excerptProjection = DefaultAccountProjection.class)
 public interface AccountRepository extends PagingAndSortingRepository<Account, Long>, CrudRepository<Account, Long>{
 }
 
