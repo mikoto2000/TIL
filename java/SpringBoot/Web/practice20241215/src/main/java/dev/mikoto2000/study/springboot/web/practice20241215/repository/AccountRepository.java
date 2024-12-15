b93971e01cb9193b8af9875cec056a96830631dd
mikoto2000 <mikoto2000@gmail.com>
2024-12-15 20:17:00 +0900

    Fixed implemented interface in AccountRepository.

--- java/SpringBoot/Web/practice20241215/src/main/java/dev/mikoto2000/study/springboot/web/practice20241215/repository/AccountRepository.java
+++ java/SpringBoot/Web/practice20241215/src/main/java/dev/mikoto2000/study/springboot/web/practice20241215/repository/AccountRepository.java
@@ -1,5 +1,6 @@
 package dev.mikoto2000.study.springboot.web.practice20241215.repository;
 
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import dev.mikoto2000.study.springboot.web.practice20241215.entity.Account;
@@ -7,6 +8,6 @@ import dev.mikoto2000.study.springboot.web.practice20241215.entity.Account;
 /**
  * AccountRepository
  */
-public interface AccountRepository extends PagingAndSortingRepository<Account, Long> {
+public interface AccountRepository extends PagingAndSortingRepository<Account, Long>, CrudRepository<Account, Long>{
 }
 
