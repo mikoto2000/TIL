97d841823f788d53b963c8f25ef2ea6f5c245b41
mikoto2000 <mikoto2000@gmail.com>
2024-12-16 18:06:24 +0900

    リポジトリにページングとソーティングを追加。

--- java/SpringBoot/Web/practice20241216/src/main/java/dev/mikoto2000/study/springboot/web/practice20241215/repository/AuthorRepository.java
+++ java/SpringBoot/Web/practice20241216/src/main/java/dev/mikoto2000/study/springboot/web/practice20241215/repository/AuthorRepository.java
@@ -1,12 +1,13 @@
 package dev.mikoto2000.study.springboot.web.practice20241215.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import dev.mikoto2000.study.springboot.web.practice20241215.entity.Author;
 
 /**
  * AuthorRepository
  */
-public interface AuthorRepository extends CrudRepository<Author, Long> {
+public interface AuthorRepository extends PagingAndSortingRepository<Author, Long>, CrudRepository<Author, Long> {
 }
 
