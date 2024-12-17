4c067f243dd72969b42f15db9abcbb1011b6a12b
mikoto2000 <mikoto2000@gmail.com>
2024-12-17 19:36:12 +0900

    書籍管理のバックエンド作成完了。

--- java/SpringBoot/Web/practice20241217/src/main/java/dev/mikoto2000/study/springboot/web/practice20241215/entity/NdcCategory.java
+++ java/SpringBoot/Web/practice20241217/src/main/java/dev/mikoto2000/study/springboot/web/practice20241215/entity/NdcCategory.java
@@ -1,38 +1,24 @@
 package dev.mikoto2000.study.springboot.web.practice20241215.entity;
 
-import java.time.LocalDate;
-import java.util.List;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToMany;
-import jakarta.persistence.ManyToOne;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 /**
- * BookMaster
+ * NdcCategory
  */
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-public class BookMaster {
+public class NdcCategory {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
-  @NotNull
   private String name;
-  @NotNull
-  private LocalDate publicationDate;
-  @NotNull
-  @ManyToOne
-  private NdcCategory ndcCategory;
-  @ManyToMany
-  private List<Author> author;
+  private Long number;
 }
-
