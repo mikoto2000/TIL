e225741225e0dd974b07c9e0c001c3b2266e56ba
mikoto2000 <mikoto2000@gmail.com>
2024-12-18 08:41:44 +0900

    でふぉるのコードを削除。

--- java/SpringBoot/Web/practice20241217/frontend/src/App.tsx
+++ java/SpringBoot/Web/practice20241217/frontend/src/App.tsx
@@ -6,8 +6,6 @@ import './App.css'
 const BASE_URL = "http://localhost:8080"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   const [authors, setAuthors] = useState([])
 
   useEffect(() => {
@@ -21,27 +19,6 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-
       <ul>
         {
           authors.map((e) => <li>{e.name}</li>)
