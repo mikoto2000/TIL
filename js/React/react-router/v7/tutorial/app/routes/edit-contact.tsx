7134f2bb0245f53aff9fedff18a6d040579161b0
mikoto2000 <mikoto2000@gmail.com>
2024-12-31 11:15:00 +0000

    Cancel で詳細画面に戻るように修正。

--- js/React/react-router/v7/tutorial/app/routes/edit-contact.tsx
+++ js/React/react-router/v7/tutorial/app/routes/edit-contact.tsx
@@ -1,4 +1,4 @@
-import { Form, redirect } from "react-router";
+import { Form, redirect, useNavigate } from "react-router";
 import type { Route } from "./+types/edit-contact";
 
 import { getContact, updateContact } from "../data";
@@ -22,6 +22,7 @@ export default function EditContact({
   loaderData,
 }: Route.ComponentProps) {
   const { contact } = loaderData;
+  const navigate = useNavigate();
 
   return (
     <Form key={contact.id} id="contact-form" method="post">
@@ -71,7 +72,7 @@ export default function EditContact({
       </label>
       <p>
         <button type="submit">Save</button>
-        <button type="button">Cancel</button>
+        <button type="button" onClick={() => navigate(-1)}>Cancel</button>
       </p>
     </Form>
   );
