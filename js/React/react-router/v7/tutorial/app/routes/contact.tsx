7c85f2078c9678409b94f1624d0689f09f7f2ddd
mikoto2000 <mikoto2000@gmail.com>
2024-12-31 11:08:42 +0000

    content の loader の null チェック追加漏れに対応。

--- js/React/react-router/v7/tutorial/app/routes/contact.tsx
+++ js/React/react-router/v7/tutorial/app/routes/contact.tsx
@@ -5,6 +5,9 @@ import type { Route } from "./+types/contact";
 
 export async function loader({ params }: Route.LoaderArgs) {
   const contact = await getContact(params.contactId);
+  if (!contact) {
+    throw new Response("Not Found", { status: 404 });
+  }
   return { contact };
 }
 
