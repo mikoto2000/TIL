8c26895109176c01e8602fe32d250e2eb7969990
mikoto2000 <mikoto2000@gmail.com>
2024-12-31 09:10:55 +0000

    新規作成時に、作成したレコードのエディット画面に遷移するように修正。

--- js/React/react-router/v7/tutorial/app/root.tsx
+++ js/React/react-router/v7/tutorial/app/root.tsx
@@ -3,6 +3,7 @@ import {
   Scripts,
   ScrollRestoration,
   isRouteErrorResponse,
+  redirect,
 } from "react-router";
 import type { Route } from "./+types/root";
 
@@ -11,7 +12,7 @@ import { createEmptyContact } from "./data";
 
 export async function action() {
   const contact = await createEmptyContact();
-  return { contact };
+  return redirect(`/contacts/${contact.id}/edit`);
 }
 
 export function HydrateFallback() {
