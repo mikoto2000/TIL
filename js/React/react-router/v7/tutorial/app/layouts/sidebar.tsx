71535cd4da99a357a445be087ce756534c0f0fdc
mikoto2000 <mikoto2000@gmail.com>
2024-12-31 09:16:04 +0000

    表示中のレコードをハイライトするように修正。
    isActive, isPending は NavLink の機能らしい。

--- js/React/react-router/v7/tutorial/app/layouts/sidebar.tsx
+++ js/React/react-router/v7/tutorial/app/layouts/sidebar.tsx
@@ -1,4 +1,4 @@
-import { Form, Link, Outlet } from "react-router";
+import { Form, Link, NavLink, Outlet } from "react-router";
 import { getContacts } from "../data";
 import type { Route } from "./+types/sidebar";
 
@@ -45,7 +45,16 @@ export default function SidebarLayout({
             <ul>
               {contacts.map((contact) => (
                 <li key={contact.id}>
-                  <Link to={`contacts/${contact.id}`}>
+                  <NavLink
+                    className={({ isActive, isPending }) =>
+                      isActive
+                        ? "active"
+                        : isPending
+                          ? "pending"
+                          : ""
+                    }
+                    to={`contacts/${contact.id}`}
+                  >
                     {contact.first || contact.last ? (
                       <>
                         {contact.first} {contact.last}
@@ -56,7 +65,7 @@ export default function SidebarLayout({
                     {contact.favorite ? (
                       <span>★</span>
                     ) : null}
-                  </Link>
+                  </NavLink>
                 </li>
               ))}
             </ul>
