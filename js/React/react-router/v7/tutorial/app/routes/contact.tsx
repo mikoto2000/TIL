383bec48f4a069c8498c9381ca9c9213e2c85e66
mikoto2000 <mikoto2000@gmail.com>
2024-12-31 06:55:39 +0000

    contact に個別の情報を表示するように修正。

--- js/React/react-router/v7/tutorial/app/routes/contact.tsx
+++ js/React/react-router/v7/tutorial/app/routes/contact.tsx
@@ -1,16 +1,15 @@
 import { Form } from "react-router";
 
-import type { ContactRecord } from "../data";
+import { getContact, type ContactRecord } from "../data";
+import type { Route } from "./+types/contact";
 
-export default function Contact() {
-  const contact = {
-    first: "Your",
-    last: "Name",
-    avatar: "https://placecats.com/200/200",
-    twitter: "your_handle",
-    notes: "Some notes",
-    favorite: true,
-  };
+export async function loader({ params }: Route.LoaderArgs) {
+  const contact = await getContact(params.contactId);
+  return { contact };
+}
+
+export default function Contact({ loaderData }: Route.ComponentProps) {
+  const { contact } = loaderData;
 
   return (
     <div id="contact">
