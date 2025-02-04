8d35a6cf216d5b58f5c662d8806acefd39adb9d7
mikoto2000 <mikoto2000@gmail.com>
2025-02-04 20:40:15 +0900

    プロジェクトの追加。
    See: https://start.spring.io/#!type=maven-project&language=java&platformVersion=3.4.2&packaging=jar&jvmVersion=17&groupId=dev.mikoto2000.study.mybatis&artifactId=multidatasource&name=multidatasource&description=MyBatis%20multi-datasource%20demo%20project%20for%20Spring%20Boot&packageName=dev.mikoto2000.study.mybatis.multidatasource&dependencies=postgresql,lombok,mybatis,devtools

---
+++ java/SpringBoot/MyBatis/multi-datasource/src/test/java/dev/mikoto2000/study/mybatis/multidatasource/MultidatasourceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mikoto2000.study.mybatis.multidatasource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultidatasourceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
