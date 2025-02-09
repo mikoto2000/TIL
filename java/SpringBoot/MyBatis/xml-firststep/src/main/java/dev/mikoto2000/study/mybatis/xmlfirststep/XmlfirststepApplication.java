06845a9628a1774042cbe4fb9deb9cf75fce0dc8
mikoto2000 <mikoto2000@gmail.com>
2025-02-09 15:56:47 +0900

    Added spring project.

---
+++ java/SpringBoot/MyBatis/xml-firststep/src/main/java/dev/mikoto2000/study/mybatis/xmlfirststep/XmlfirststepApplication.java
@@ -0,0 +1,13 @@
+package dev.mikoto2000.study.mybatis.xmlfirststep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmlfirststepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XmlfirststepApplication.class, args);
+	}
+
+}
---
+++ java/SpringBoot/MyBatis/xml-firststep/src/test/java/dev/mikoto2000/study/mybatis/xmlfirststep/XmlfirststepApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mikoto2000.study.mybatis.xmlfirststep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XmlfirststepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
