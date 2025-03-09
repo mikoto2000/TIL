42fddd90fc4ff70ecbdb76791559f15177edf377
mikoto2000 <mikoto2000@gmail.com>
2025-03-09 09:58:20 +0900

    jakarta への変更を考慮していなかった問題を修正。

--- java/Servlet/firststep/firststep/src/main/java/dev/mikoto2000/study/servlet/firststep/HelloServlet.java
+++ java/Servlet/firststep/firststep/src/main/java/dev/mikoto2000/study/servlet/firststep/HelloServlet.java
@@ -1,14 +1,15 @@
 package dev.mikoto2000.study.servlet.firststep;
 
 import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @WebServlet("/hello")
 public class HelloServlet extends HttpServlet {
+  @Override
   protected void doGet(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {
       response.setContentType("text/html;charset=UTF-8");
