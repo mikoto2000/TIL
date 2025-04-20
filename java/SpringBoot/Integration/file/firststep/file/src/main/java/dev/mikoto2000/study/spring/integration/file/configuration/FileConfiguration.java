5015144ed860f94f8f019886d7ee92d39b18c170
mikoto2000 <mikoto2000@gmail.com>
2025-04-20 21:04:56 +0900

    ファイル出力フローを追加。

--- java/SpringBoot/Integration/file/firststep/file/src/main/java/dev/mikoto2000/study/spring/integration/file/configuration/FileConfiguration.java
+++ java/SpringBoot/Integration/file/firststep/file/src/main/java/dev/mikoto2000/study/spring/integration/file/configuration/FileConfiguration.java
@@ -4,12 +4,17 @@ import java.io.File;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.channel.DirectChannel;
 import org.springframework.integration.config.EnableIntegration;
 import org.springframework.integration.dsl.IntegrationFlow;
 import org.springframework.integration.dsl.Pollers;
+import org.springframework.integration.file.FileWritingMessageHandler;
 import org.springframework.integration.file.dsl.Files;
 import org.springframework.integration.file.filters.FileSystemMarkerFilePresentFileListFilter;
 import org.springframework.integration.file.filters.SimplePatternFileListFilter;
+import org.springframework.integration.file.support.FileExistsMode;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.MessageHandler;
 
 /**
  * SftpConfiguration
@@ -28,7 +33,54 @@ public class FileConfiguration {
         .handle(m -> {
           // 受信したファイルの処理
           System.out.println("Received: " + ((File) m.getPayload()).getName());
+
+          // 出力用チャネルにメッセージを送信
+          fileOutboundChannel().send(m);
         })
         .get();
   }
+
+  /**
+   * ファイル出力用チャネル。
+   */
+  @Bean
+  public MessageChannel fileOutboundChannel() {
+    return new DirectChannel();
+  }
+
+  /**
+   * ファイル出力用ハンドラ。
+   *
+   * 受信したファイルを指定のディレクトリに書き込む。
+   * 受信元ファイルと、 .complete ファイルを削除する。
+   */
+  @Bean
+  public MessageHandler fileOutboundHandler() {
+    return message -> {
+      // 定義済みのハンドラを利用して、受信したファイルを指定のディレクトリに書き込む
+      FileWritingMessageHandler handler = new FileWritingMessageHandler(new File("./dir/receivecompany"));
+      handler.setFileExistsMode(FileExistsMode.REPLACE);
+      handler.setExpectReply(false);
+
+      // 受信元ファイルと、 .complete ファイルを削除する
+      File file = (File) message.getPayload();
+      file.delete();
+      File completeFile = new File(file.getAbsolutePath() + ".complete");
+      completeFile.delete();
+    };
+  }
+
+  /**
+   * ファイル出力用 IntegrationFlow。
+   *
+   * 受信したファイルを指定のディレクトリに書き込む。
+   */
+  @Bean
+  public IntegrationFlow fileOutboundFlow() {
+    return IntegrationFlow
+        .from(fileOutboundChannel())
+        .handle(fileOutboundHandler())
+        .get();
+  }
+
 }
