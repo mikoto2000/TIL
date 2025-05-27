6f1adf1f071a25a8d4b32ed1c5f7d7d9ed63621c
mikoto2000 <mikoto2000@gmail.com>
2025-05-27 14:57:03 +0000

    Added `java/sns/mastodon/firststep`.

---
+++ java/sns/mastodon/firststep/src/main/java/dev/mikoto2000/study/sns/mastodon/App.java
@@ -0,0 +1,10 @@
+package dev.mikoto2000.study.sns.mastodon;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
