da1bfbf47b5852e9f92bbd0be6e2f623c4496545
mikoto2000 <mikoto2000@gmail.com>
2025-08-10 10:09:56 +0900

    カレンダーを二つ表示するように修正。

--- js/vanilla/calendar/index.mjs
+++ js/vanilla/calendar/index.mjs
@@ -3,18 +3,22 @@ import { createCalendarView } from './calendar.mjs';
 document.addEventListener('DOMContentLoaded', function() {
 
   // カレンダーの div を取得
-  const calendarDiv = document.getElementById('calendar');
-  calendarDiv.className = 'calendarContainer';
+  const calendarDiv1 = document.getElementById('calendarContainer1');
+  calendarDiv1.className = 'calendarContainer1';
+  const calendarDiv2 = document.getElementById('calendarContainer2');
+  calendarDiv2.className = 'calendarContainer2';
 
   // カレンダーの div が存在する場合のみ処理を続ける
-  if (!calendarDiv) {
+  if (!calendarDiv1 || !calendarDiv2) {
     console.error('Calendar div not found');
     return;
   }
 
   const currentDate = new Date();
-  const calendar = createCalendarView(currentDate.getFullYear(), currentDate.getMonth());
-  calendarDiv.appendChild(calendar);
+  const calendar1 = createCalendarView(currentDate.getFullYear(), currentDate.getMonth());
+  calendarDiv1.appendChild(calendar1);
+  const calendar2 = createCalendarView(currentDate.getFullYear(), currentDate.getMonth() + 1);
+  calendarDiv1.appendChild(calendar2);
 });
 
 
