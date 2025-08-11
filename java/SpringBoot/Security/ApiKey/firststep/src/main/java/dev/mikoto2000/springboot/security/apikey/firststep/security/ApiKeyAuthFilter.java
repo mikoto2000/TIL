ca75b71a8927422098b5d69df59c5222b01ac6c4
mikoto2000 <mikoto2000@gmail.com>
2025-08-11 04:03:32 +0000

    API Key 処理用のフィルタを追加。

---
+++ java/SpringBoot/Security/ApiKey/firststep/src/main/java/dev/mikoto2000/springboot/security/apikey/firststep/security/ApiKeyAuthFilter.java
@@ -0,0 +1,67 @@
+package dev.mikoto2000.springboot.security.apikey.firststep.security;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class ApiKeyAuthFilter extends OncePerRequestFilter {
+
+  private static final String API_KEY_HEADER = "x-api-key";
+
+  private final String clientName;
+  private final String apiKey;
+
+  public ApiKeyAuthFilter(String clientName, String apiKey) {
+    this.clientName = clientName;
+    this.apiKey = apiKey;
+  }
+
+  @Override
+  protected void doFilterInternal(HttpServletRequest request,
+      HttpServletResponse response,
+      FilterChain chain) throws IOException, ServletException {
+
+    // CORSプリフライトは素通し（必要に応じて）
+    if (HttpMethod.OPTIONS.matches(request.getMethod())) {
+      chain.doFilter(request, response);
+      return;
+    }
+
+    // 既に認証済みならスキップ
+    Authentication current = SecurityContextHolder.getContext().getAuthentication();
+    if (current != null && current.isAuthenticated()) {
+      chain.doFilter(request, response);
+      return;
+    }
+
+    // API Key による認証
+    String requestApiKey = request.getHeader(API_KEY_HEADER);
+    if (StringUtils.hasText(apiKey) && StringUtils.hasText(requestApiKey) && apiKey.equals(requestApiKey)) {
+      // API クライアント用トークン作成
+      var token = new UsernamePasswordAuthenticationToken(
+          clientName,
+          "",
+          List.of(new SimpleGrantedAuthority(String.format("ROLE_%s", clientName))));
+      token.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+      // SecurityContextHolder に認証済みトークンを設定
+      SecurityContextHolder.getContext().setAuthentication(token);
+    }
+
+    // 次のフィルタへ
+    chain.doFilter(request, response);
+  }
+}
